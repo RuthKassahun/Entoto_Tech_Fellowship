25f0ae0b23459fcad45fa63f2352639f1d809cb7
RuthKassahun <ruthkehali@gmail.com>
2019-06-14 16:52:13 +0300

    add

---
+++ week1/Day4/day4.js
@@ -0,0 +1,14 @@
+var book ={
+  title: "The diary of a wimpy kid",
+  author: "George Jeff",
+  releaseDate: 2014,
+}
+  console.log(book)
+  console.log(book.title)
+
+var food = ["cake", "pizza", "tibs"]
+console.log (food)
+
+for (i=0; i<3; i++){
+  console.log (food[i])
+}
